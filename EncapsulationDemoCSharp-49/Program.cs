fea9849aaa2bb455a6aed60249095b8709e0174e
SethBowman <brandonsethbowman@gmail.com>
2024-07-18 13:44:53 -0500

    demo completed

---
+++ EncapsulationDemoCSharp-49/Program.cs
@@ -0,0 +1,14 @@
+﻿using EncapsulationDemoCSharp_49;
+
+var seth = new Person();
+
+seth.AddMoney(100.00);
+
+var currentWallet = seth.CheckWallet();
+Console.WriteLine(currentWallet);
+
+seth.AddMoney(200.00);
+
+currentWallet = seth.CheckWallet();
+Console.WriteLine(currentWallet);
+
